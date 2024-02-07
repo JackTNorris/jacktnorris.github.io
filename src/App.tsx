425c67c10948742dc3495aa2c02fcf5aea6d6717
Jack Norris <jacktimothynorris@gmail.com>
2024-02-07 00:48:11 -0600

    set home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
 import Home from './screens/Home';
 import { Header } from './components/Header';
@@ -13,6 +13,7 @@ function App() {
       <Header/>
         <BrowserRouter basename='/'>
           <Routes>
+            <Route path='/' element={<Navigate to={'/home'} />} />
             <Route path="/home" Component={Home} />
             <Route path="/blog" Component={Blog} />
           </Routes>
