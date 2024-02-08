bb59dcbd332a3b4536c168056e88a8fb121e24d4
Jack Norris <jacktimothynorris@gmail.com>
2024-02-07 23:03:48 -0600

    tentative private routes

---
+++ src/components/PrivateRoutes.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { auth as fbauth } from '../loaders/firebase'
+export const PrivateRoutes = () => {
+  let auth = {'token': !!fbauth.currentUser}
+return (
+    auth.token ? <Outlet/> : <Navigate to='/login'/>
+  )
+}
