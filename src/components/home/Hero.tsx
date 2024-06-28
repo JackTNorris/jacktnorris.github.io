13aa2ded74a829005f65239f3affcf1840d5ca91
Jack Norris <jacktimothynorris@gmail.com>
2024-06-27 22:25:34 -0400

    small adjustment to page

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,4 @@
+//TODO: migrate completely over to vanilla html, css, and js
 import ProfilePicture from '../../assets/images/pfp.jpeg';
 import { TextCarousel } from './TextCarousel';
 
@@ -7,7 +8,7 @@ export const Hero = () => {
             <h1 className='text-3xl font-extrabold p-3'>Hi! I'm Jack</h1>
             <img src={ProfilePicture} alt='me' className='rounded-full w-96 object-scale-down p-3'/>
             {/*<h3 className='text-xl font-extrabold underline p-3'>I'm a</h3>*/}
-            <TextCarousel items={['Student', 'Developer', 'Violinist', 'Outdoors-Lover']}/>
+            <TextCarousel items={['Peace Corps Volunteer', 'Developer', 'Violinist', 'Outdoors-Lover']}/>
         </div>
     )
 }
