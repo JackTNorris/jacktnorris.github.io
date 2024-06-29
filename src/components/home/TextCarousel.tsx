1d8efbd7581cfe7f6dd657477a9c7559e742dc64
Jack Norris <jacktimothynorris@gmail.com>
2024-06-28 22:08:34 -0400

    fixed formatting of pcv

--- src/components/home/TextCarousel.tsx
+++ src/components/home/TextCarousel.tsx
@@ -7,7 +7,7 @@ export const TextCarousel = ({items}: TextCarouselProps) => {
     const [currentItem, setCurrentItem] = useState(0)
     const tempItems = items.slice(0).concat(items).concat(items.slice(0, 1))
     return (
-    <div className='w-48 h-8 overflow-hidden text-center'>
+    <div className='w-64 h-8 overflow-hidden text-center'>
         {tempItems.map((item, index) => <p key={index} className='text-2xl text-blue-500 font-bold animate-snap-slide-down'>{item}</p>)}
     </div>)
 }
