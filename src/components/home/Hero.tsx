78104ed07ea707fd84623afea47824f24138c53a
Jack Norris <jacktimothynorris@gmail.com>
2024-06-29 21:37:17 -0400

    adjusted dev title

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
             <h1 className='text-3xl font-extrabold p-3 text-center'>Hi! I'm Jack</h1>
             <img src={ProfilePicture} alt='me' className='rounded-full w-96 object-scale-down p-3'/>
             {/*<h3 className='text-xl font-extrabold underline p-3'>I'm a</h3>*/}
-            <TextCarousel items={['Peace Corps Volunteer', 'Developer', 'Violinist', 'Outdoors-Lover']}/>
+            <TextCarousel items={['Peace Corps Volunteer', 'Software Developer', 'Violinist', 'Outdoors-Lover']}/>
         </div>
     )
 }
