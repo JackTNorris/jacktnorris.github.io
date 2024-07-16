88d95994d43e46718fb5f7005cb3e3e0beaf00ec
Jack Norris <jacktimothynorris@gmail.com>
2024-07-15 21:51:39 -0400

    fixe some styling

--- src/components/blog/BlogFeed.tsx
+++ src/components/blog/BlogFeed.tsx
@@ -21,7 +21,7 @@ export const BlogFeed = ({topic}: BlogFeedProps) => {
     return (
         <div className='flex flex-col w-3/5 min-w-[20rem]'>
             {blogs.map((blog, index) => 
-            <div className={twMerge('w-4/5 aspect-[5/2] mt-12 bg-slate-100 shadow-md rounded-lg', index % 2 ? 'self-end' : 'self-start')}>
+            <div className={twMerge('w-4/5 aspect-[5/2] mt-12 bg-slate-100 shadow-md rounded-lg p-8', index % 2 ? 'self-end' : 'self-start')}>
                 <p><em><b>{blog.title}</b> || {new Date(blog.createdOn).toDateString()}</em></p>
                 <Markdown skipHtml={false} rehypePlugins={[rehypeRaw]} className='prose'>{blog.content}</Markdown>
             </div>)}
