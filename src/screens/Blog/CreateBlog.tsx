04121550f3d89300f123db50d031b9a6d1f0282f
Jack Norris <jacktimothynorris@gmail.com>
2024-07-17 20:23:20 -0400

    fixed some styles, need to clean up html

--- src/screens/Blog/CreateBlog.tsx
+++ src/screens/Blog/CreateBlog.tsx
@@ -56,6 +56,7 @@ export const CreateBlog = () => {
                         <option key={index} value={category}>{category}</option>
                     ))}
                 </select>
+                <div className='w-full max-w-[50rem] font-bold'>
                 <div className='w-full max-w-[50rem] font-bold'><p>Content: </p></div>
                 <MDXEditorWrapper className='-z-10 prose w-full p-1 max-w-[50rem] h-96 border rounded-md overflow-scroll' onChange={val => setFormValue({...formValue, content: val})} />
                 <div className='w-full max-w-[50rem] font-bold'><p>Result: </p></div>
@@ -64,6 +65,7 @@ export const CreateBlog = () => {
                     rehypePlugins={[rehypeRaw]} 
                     className='prose w-full p-1 max-w-[50rem] h-96 border rounded-md overflow-scroll'
                 >{formValue.content}</Markdown>
+                </div>
                 <button className='w-full max-w-[50rem] h-8 border-blue-500 hover:bg-blue-200 border text-black rounded-md transition-all' onClick={onPressSave}>Save</button>
                 <button className='w-full max-w-[50rem] h-8 bg-blue-500 hover:bg-blue-400 text-white rounded-md transition-all' onClick={onPressPublish}>Publish</button>
             </div>
