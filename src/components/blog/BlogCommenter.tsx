a1f5a4d7767747b05f1e3022895b49139bb6eb72
Jack Norris <jacktimothynorris@gmail.com>
2024-09-04 16:13:36 -0400

    Fixed some styling for blog posts

--- src/components/blog/BlogCommenter.tsx
+++ src/components/blog/BlogCommenter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const BlogCommenter = () => {
     return (
         <div className='w-full'>
-            
+                        
         </div>
     );
 }
--- src/components/blog/BlogFeed.tsx
+++ src/components/blog/BlogFeed.tsx
@@ -51,9 +51,9 @@ export const BlogFeed = ({topic, isDrafts}: BlogFeedProps) => {
     }, [])
     console.log(blogs)
     return (
-        <div className='flex flex-col items-center w-3/5 min-w-[20rem] pb-8'>
+        <div className='flex flex-col items-center w-3/5 min-w-[20rem] pb-8 gap-12'>
             {blogs.map((blog, index) => 
-                <div className={twMerge('flex flex-col gap-4 w-4/5 aspect-[5/2] mt-12 bg-slate-100 shadow-md rounded-lg p-8 transition-all duration-1000')}>
+                <div className={twMerge('flex flex-col w-4/5 aspect-[5/2] bg-slate-100 shadow-md rounded-lg p-8 transition-all duration-1000')}>
                     <div className="flex flex-row justify-between items-center flex-1">
                         <p><em><b>{blog.title}</b> || {new Date(blog.createdOn).toDateString()} </em></p>
                         <div className={twMerge("flex flex-row", auth.currentUser ? 'visible' : 'hidden')}>
