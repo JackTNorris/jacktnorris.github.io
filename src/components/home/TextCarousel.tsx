ce56187878f1a9983db107a3b19e8638bfa3e636
Jack Norris <jacktimothynorris@gmail.com>
2024-09-23 22:03:58 -0400

    fixed issues with max rendering oddly

--- src/components/home/TextCarousel.tsx
+++ src/components/home/TextCarousel.tsx
@@ -8,6 +8,6 @@ export const TextCarousel = ({items}: TextCarouselProps) => {
     const tempItems = items.slice(0).concat(items).concat(items.slice(0, 1))
     return (
     <div className='w-96 h-8 overflow-hidden text-center -z-10'>
-        {tempItems.map((item, index) => <p key={index} className='press-start text-lg font-extrabold animate-snap-slide-down text-blue text-blue-500'>{item}</p>)}
+        {tempItems.map((item, index) => <p key={index} className='press-start py-3 font-extrabold animate-snap-slide-down text-blue text-blue-500'>{item}</p>)}
     </div>)
 }
