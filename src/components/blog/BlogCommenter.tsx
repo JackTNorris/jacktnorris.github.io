821ba9d697dae761dc8870052664255d241dcc5c
Jack Norris <jacktimothynorris@gmail.com>
2024-10-12 21:49:44 -0300

    state variable for comments

--- src/components/blog/BlogCommenter.tsx
+++ src/components/blog/BlogCommenter.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const BlogCommenter = () => {
+    const [setComment, comment] = useState<string>('');
     return (
         <div className='w-full'>
-                        
+            
         </div>
     );
 }
