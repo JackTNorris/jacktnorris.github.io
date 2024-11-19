29d9028bdbf557a3af80b86caf1ab8208bda603c
Jack Norris <jacktimothynorris@gmail.com>
2024-11-19 14:51:28 -0300

    added in hyperlinks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export const Footer = () => {
     <div className="flex gap-3 flex-col justify-center items-center w-screen h-36 bg-white shadow-[0_35px_80px_-15px_rgba(0,0,0,0.3)]">
         <p className='bottom-border px-9 text-xs'>Built by Jack Norris</p>
         <div className='flex flex-row gap-4 justify-center'>
-            <a><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={github} /></a>
-            <a><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={linkedin} /></a>
+            <a href="https://github.com/JackTNorris"><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={github} /></a>
+            <a href="https://www.linkedin.com/in/jack-t-norris/"><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={linkedin} /></a>
         </div>
     </div>
     );
