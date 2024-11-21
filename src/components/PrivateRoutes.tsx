a4aeb1a773678a0634016cc3f98d5b987504a331
Jack Norris <jacktimothynorris@gmail.com>
2024-11-20 23:40:40 -0300

    fixed weird loader issue

--- src/components/PrivateRoutes.tsx
+++ src/components/PrivateRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth';
+import { Loader } from './Loader';
 export const PrivateRoutes = () => {
   const user = useAuth();
   return typeof user === 'undefined' ? (
-    <h1>Loading.....</h1>
+    <Loader />
   ) : user ? (
     <Outlet />
   ) : (
