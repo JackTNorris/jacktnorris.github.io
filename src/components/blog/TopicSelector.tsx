c3bcc08a1770a916c59f7e8ff4b07d3b09e51896
Jack Norris <jacktimothynorris@gmail.com>
2025-01-12 23:30:43 -0300

    styling updates

--- src/components/blog/TopicSelector.tsx
+++ src/components/blog/TopicSelector.tsx
@@ -11,15 +11,17 @@ export const TopicSelector = ({onSelectTopic, topics}: TopicSelectorProps) => {
     const [isOpen, setIsOpen] = useState(false)
     const [currTopic, setCurrTopic] = useState('🕊️ peace corps')
     return topics.length ? (
-    <div className="flex flex-col gap-2 mt-8 absolute">
-        <button className="select-button border w-52 p-2 rounded-lg" onClick={() => setIsOpen(!isOpen)}>
-            <span className='flex flex-row items-center justify-between'>
-                <span className="selected-value">{currTopic.split(' ').map(i => capitalize(i)).join(' ')}</span>
-                <span className="arrow"><img className={twMerge('transition-all h-3 w-3', isOpen && 'rotate-180')} src={chevron} /></span>
-            </span>
-        </button>
-        <div className={twMerge('transition-all shadow-md w-52 bg-white rounded-xl h-0 overflow-hidden', isOpen && 'border h-36 overflow-scroll')}>
-            {topics.map(topic => <div onClick={() => {setCurrTopic(topic); onSelectTopic(topic); setIsOpen(false)} } className='py-2 border hover:bg-slate-200 hover:cursor-pointer'>{topic.split(' ').map(i => capitalize(i)).join(' ') }</div>)}
+    <div className="flex flex-row justify-center h-26 my-6">
+        <div className='h-26 absolute'>
+            <button className="border w-52 p-2 rounded-lg" onClick={() => setIsOpen(!isOpen)}>
+                <span className='flex flex-row items-center justify-between'>
+                    <span className="selected-value">{currTopic.split(' ').map(i => capitalize(i)).join(' ')}</span>
+                    <span className="arrow"><img className={twMerge('transition-all h-3 w-3', isOpen && 'rotate-180')} src={chevron} /></span>
+                </span>
+            </button>
+            <div className={twMerge('transition-all shadow-md w-52 bg-white rounded-xl h-0 overflow-hidden', isOpen && 'border h-36 overflow-scroll')}>
+                {topics.map(topic => <div onClick={() => {setCurrTopic(topic); onSelectTopic(topic); setIsOpen(false)} } className='py-2 border hover:bg-slate-200 hover:cursor-pointer'>{topic.split(' ').map(i => capitalize(i)).join(' ') }</div>)}
+            </div>
         </div>
     </div>
     ) : <></>
