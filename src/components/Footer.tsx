d20a44ec0e9e58b613f81553fbd5d7e0e1162f8f
Jack Norris <jacktimothynorris@gmail.com>
2025-01-17 14:39:29 -0300

    added instagram link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import github from 'assets/images/socials/github.png';
 import linkedin from 'assets/images/socials/linkedin.png';
-
+import instagram from 'assets/images/socials/instagram.png';
 export const Footer = () => {
     return (
     <div className="flex gap-3 flex-col justify-center items-center w-screen h-36 bg-white shadow-[0_35px_80px_-15px_rgba(0,0,0,0.3)]">
@@ -8,6 +8,8 @@ export const Footer = () => {
         <div className='flex flex-row gap-4 justify-center'>
             <a href="https://github.com/JackTNorris"><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={github} /></a>
             <a href="https://www.linkedin.com/in/jack-t-norris/"><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={linkedin} /></a>
+            <a href="https://www.instagram.com/jacktnorris_01/"><img className='w-8 transition-all h-8 hover:opacity-25 hover:cursor-pointer hover:-translate-y-1' src={instagram} /></a>
+
         </div>
     </div>
     );
