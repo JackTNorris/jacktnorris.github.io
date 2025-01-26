88edaa09f3d2b90e651454e2c74f258394945a13
Jack Norris <jacktimothynorris@gmail.com>
2025-01-26 00:19:30 -0300

    more responsive homepage

--- src/components/home/AboutMe.tsx
+++ src/components/home/AboutMe.tsx
@@ -1,27 +1,27 @@
 import SkydivePicture from '../../assets/images/skydive.jpeg'
 export const AboutMe = () => {
     return (
-        <div className='flex-col sm:flex-row flex w-screen sm:h-screen px-8 py-10 sm:px-36 gap-3'>
+        <div className='flex-col lg:flex-row flex w-screen md:h-screen px-8 py-10 lg:px-36 gap-3'>
             <div className='flex flex-col h-full w-full justify-center items-center gap-3'>
-                <div className='flex w-full flex-row justify-center sm:justify-start'>
-                    <h1 className='text-lg sm:text-3xl font-extrabold press-start text-blue-500'>A little about me...</h1>
+                <div className='flex w-full flex-row justify-center md:justify-start'>
+                    <h1 className='text-lg md:text-3xl font-extrabold press-start text-blue-500'>A little about me...</h1>
                 </div>
                 <div className='flex flex-col gap-4'>
                     <p className='font-bold text-sm sm:text-xl'>
-                    I'm currently a Peace Corps Health Volunteer in Paraguay, putting into practice my belief that service is a responsibility that extends beyond the domain of doctors and social workers -- it's also the duty of scientists and engineers.
+                    At the moment, I'm serving as a Peace Corps Health Volunteer in rural Paraguay, putting into practice my belief that service is a responsibility that extends beyond the domain of doctors and social workers -- it's the duty of scientists and engineers as well.
                     </p>
                     <p className='font-bold text-sm sm:text-xl'>
-                    In my freetime, I love travelling, adventuring outside, playing my violin, reading, or writing code!
+                    When I'm not writing code, I love adventuring outside, tinkering with small electronics, or fiddling around with my violin. 
                     </p>
                     <p className='font-bold text-sm sm:text-xl'>
-                    Upon to my return to the states, I plan to attend graduate school for computer science. This site is a work in progress, but feel free to poke around!
+                    Upon to my return to the states, I plan to attend graduate school for computer science. This site is a pet project during service (and therefore, a work and progress). Feel free to poke around!
                     </p>
                 </div>
             </div>
-            <div className='h-full w-full flex flex-col justify-center items-center sm:items-end'>
-                <div className='flex flex-col justify-center items-center relative h-56 aspect-square rotate-0 transition-all sm:h-96'>
-                <div className='rounded-lg absolute translate-y-5 translate-x-5 border-2 border-blue-400 h-56 aspect-square -z-10 sm:h-96'></div>
-                    <img alt='Me skydiving' src={SkydivePicture} className='h-56 aspect-square grayscale hover:grayscale-0 hover:translate-x-3 hover:translate-y-3 rounded-lg transition-all sm:h-96 shadow-[35px_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-blue-500' />
+            <div className='h-full w-full flex flex-col justify-center items-center lg:items-end'>
+                <div className='flex flex-col justify-center items-center relative h-56 aspect-square rotate-0 transition-all md:h-96'>
+                <div className='rounded-lg absolute translate-y-5 translate-x-5 border-2 border-blue-400 h-56 aspect-square -z-10 md:h-96'></div>
+                    <img alt='Me skydiving' src={SkydivePicture} className='aspect-square grayscale hover:grayscale-0 hover:translate-x-3 hover:translate-y-3 rounded-lg transition-all md:h-96 shadow-[35px_35px_60px_-15px_rgba(0,0,0,0.3)] shadow-blue-500' />
                 </div>
             </div>
         </div>
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -12,14 +12,14 @@ export type HeroProps = {
 export const Hero = ({onSelectBackground, currBackground}: HeroProps) => {
     const backgroundOptions = [{name: 'boids', icon: '🐦‍⬛'}, {name: 'conway', icon: '👾'}, {name: 'tbp', icon: '☄️'}];
     return (
-        <div className='-mt-20 flex flex-col w-screen min-h-screen items-center justify-center'>
+        <div className='-mt-20 pt-20 flex flex-col flex-1 w-screen min-h-screen items-center justify-center'>
             <div className='flex p-4 flex-row justify-end items-end top-20 pb-20 left-0 -z-5 absolute w-full min-h-screen'>
                 <div className='p-2 flex flex-row gap-2 text-xl'>
                     {backgroundOptions.map(item => (<button className={twMerge('rounded-md transition-all focus:outline-none hover:-translate-y-2', item.name == currBackground && 'bg-blue-300')} onClick={() => onSelectBackground(item.name)}>{item.icon}</button>))}
                 </div>
             </div>
             <h1 className='press-start text-xl md:text-2xl font-extrabold p-3 text-center'>Hi, I'm Jack</h1>
-            <img src={ProfilePicture} alt='me' className='rounded-full w-[30rem] object-scale-down p-3'/>
+            <img src={ProfilePicture} alt='me' className='rounded-full max-h-[50vh] aspect-square p-3'/>
             {/*<h3 className='text-xl font-extrabold underline p-3'>I'm a</h3>*/}
             <TextCarousel items={['Peace Corps Volunteer', 'Software Developer', 'Violinist', 'Outdoors-Lover']}/>
         </div>
