a381da83fefc74488582c3b80f00c25efae61ee1
Jack Norris <jacktimothynorris@gmail.com>
2025-01-25 23:40:58 -0300

    added some responsiveness to carousel

--- src/components/home/TextCarousel.tsx
+++ src/components/home/TextCarousel.tsx
@@ -7,7 +7,7 @@ export const TextCarousel = ({items}: TextCarouselProps) => {
     const [currentItem, setCurrentItem] = useState(0)
     const tempItems = items.slice(0).concat(items).concat(items.slice(0, 1))
     return (
-    <div className='w-96 h-8 overflow-hidden text-center -z-10'>
-        {tempItems.map((item, index) => <p key={index} className='press-start py-3 font-extrabold animate-snap-slide-down text-blue text-blue-500'>{item}</p>)}
+    <div className='w-80 h-8 md:w-[30rem] m:h-12 overflow-hidden text-center -z-10'>
+        {tempItems.map((item, index) => <p key={index} className='press-start text-sm md:text-xl py-3 md:py-2 font-extrabold animate-snap-slide-down text-blue text-blue-500'>{item}</p>)}
     </div>)
 }
