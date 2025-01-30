055d1459b70b235ce80dab4491bac8fb12a617fe
Jack Norris <jacktimothynorris@gmail.com>
2025-01-30 20:22:48 -0300

    added song title

--- src/components/animations/SpotifyPlayer.tsx
+++ src/components/animations/SpotifyPlayer.tsx
@@ -1,8 +1,9 @@
+// TODO: bend it like beckham
 import { useEffect, useState } from "react";
+import { Router } from "react-router-dom";
 import { twMerge } from "tailwind-merge"
 
 export const SpotifyPlayer = () => {
-
     useEffect(() => {
         const getTrack = async () => {
             const temp = await fetch('https://api.jacktnorris.dev/spotify/getCurrentTrack')
@@ -15,19 +16,17 @@ export const SpotifyPlayer = () => {
 
     const [currentRecord, setCurrentRecord] = useState<{name?: string, image?: string, link?: string}>({})
     const [hoverRecord, setHoverRecord] = useState(false);
-    return (
+    
+    return currentRecord.name ? (
         <div className="md:flex flex-col gap-3 w-36 hidden md:visible items-start">
             <p className="press-start text-sm">What I'm Listening To:</p>
             <div className="h-24 w-24">
-                {currentRecord.name ? 
-                <>
-                    <div className={twMerge('transition-all absolute flex flex-row', hoverRecord && 'ml-14')}>
+                    <div className={twMerge('transition-all absolute flex flex-row items-center', hoverRecord && 'ml-14')}>
                         <img className={twMerge("transition-all mt-2 h-20 w-20", hoverRecord && 'animate-spin')} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/LP_Vinyl_Symbol_Icon.png/1200px-LP_Vinyl_Symbol_Icon.png" />
+                        <p className={twMerge("h-20 text-wrap text-center text-xs opacity-0", hoverRecord && 'opacity-100')} style={{textOrientation: 'mixed', writingMode: 'vertical-rl'}}>{currentRecord.name}</p>
                     </div>
-                    <a href={currentRecord.link} target="_blank"><img onMouseLeave={() => setHoverRecord(false)} onMouseEnter={() => setHoverRecord(true)} className="transition-all absolute h-24 w-24 rounded-lg shadow-2xl shadow-blue-300 hover:scale-105 hover:cursor-pointer" src={currentRecord?.image} /></a>
-                </>
-                : []}
+                    <img onMouseLeave={() => setHoverRecord(false)} onMouseEnter={() => setHoverRecord(true)} className="transition-all absolute h-24 w-24 rounded-lg shadow-2xl shadow-blue-300 hover:scale-105 hover:cursor-pointer" src={currentRecord?.image} />
             </div>
         </div>
-    )
+    ) : <></>
 }
