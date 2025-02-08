70c7c7db08ac08cb85cf5eed07e44ec6de595e18
Jack Norris <jacktimothynorris@gmail.com>
2025-02-08 18:17:56 -0300

    fixed up toast message display

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 // TODO: fix "any"
 export const ToastContainer = ({messages}: {messages: {title: string, message: string, type: 'success' | 'error' | 'warning', id: number, visible: boolean}[]}) => {
     return messages.length > 0 ? (
-        <div className='absolute top-0 left-0 flex flex-row mt-8 justify-center h-screen w-screen z-10 bg-transparent pointer-events-none'>
+        <div className='absolute top-0 left-0 flex flex-row mt-8 justify-center h-screen w-screen z-20 bg-transparent pointer-events-none'>
             <div className='transition-all flex flex-1 flex-col gap-3 items-center fixed'>
                 {messages.map((message) => 
                     <div className={twMerge('p-3 transition-all animate-fade-in w-80 h-fit overflow-clip bg-white shadow-lg  rounded-lg border border-gray-300 shadow-blue-500', !message.visible && 'animate-fade-out', message.type === 'error' && 'shadow-red-400', message.type === 'warning' && 'shadow-yellow-200')}>
