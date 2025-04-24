b3fb674018058982d38cd1dd539356d8cc5ef641
Jack Norris <jacktimothynorris@gmail.com>
2025-04-24 15:30:09 -0300

    gutted some css for js

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
 import logo from '../assets/images/logo.png';
 import { NavigationMenu } from './NavigationMenu';
 import './Header.css'
+import { useRef } from 'react';
+
 export const Header = () => {
+    const progressScroll = useRef<HTMLDivElement>(null);
     window.addEventListener('scroll', () => {
-        document.body.style.setProperty('--scroll', Math.min((window.scrollY / (document.body.offsetHeight - window.innerHeight)), 0.999999) + '');
-      }, false);
+        if (progressScroll.current)
+        {
+            const scrollTop = window.scrollY;
+            const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+            const scrollPercent = (scrollTop / docHeight) * 100;
+            progressScroll.current.style.width = scrollPercent + "%"
+        }
+      });
     return (
     <div>
         <div className='bg-white absolute flex flex-col w-screen items-center justify-center sm:justify-start h-20 shadow-lg gap-2 z-20'>
-            <div className='progress'></div>
+            <div ref={progressScroll} className='progress'></div>
             <div className='flex flex-row w-full'>
                 <a className='z-10' href='/login'><img className='h-20 w-20 sticky hover:rotate-6 transition-all' src={logo} alt='my-logo'/></a>
                 <NavigationMenu />
